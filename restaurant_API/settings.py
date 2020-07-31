19c0f3c668292f7b30235b24b6bd1de8ec7f6bdf
ikswary <ikswary@naver.com>
2020-07-31 11:58:26 +0900

    add : restaurant app and urls setting

--- restaurant_API/settings.py
+++ restaurant_API/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'zlv^hkeylnr0svnxq=f++*eghtp_arkc-5zi$rblz7cw4pbcoh'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'restaurant'
 ]
 
 MIDDLEWARE = [
